af2836de92263d59c64f0e0b0877effd3c18734d
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-10-09 16:35:11 -0300

    fix: update components name

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MarketingApp from "./components/MArketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 export default function App() {
   return (
