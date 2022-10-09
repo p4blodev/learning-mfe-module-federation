63b05aab482488a67a2a23ea75c9d88c1455ab30
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-10-09 19:23:34 -0300

    fix: update webpack prod file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>¡¡¡ CONTAINER 2.2!!!</h1>
+      <h1>¡¡¡ CONTAINER 2.3!!!</h1>
       <hr />
       <MarketingApp />
     </div>
